1e0d2042289afdf76f1546360206cf654e2b1b60
DiederichSolis <sol22952@uvg.edu.gt>
2024-08-22 13:03:17 -0600

    feat: cambios del form

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -6,7 +6,7 @@ import ContactoForm from "./ContactoForm/ContactoForm";
 const Contacto = ({ language }) =>{
     return (
         <section className="contacto-contenedor">
-            <h5>{language === 'es' ? 'Contacto' : 'Contact Me'}</h5>
+            <h5>{language === 'es' ? 'Contactanos' : 'Contact Us'}</h5>
 
             <div className="contacto-cont">
                 <div style={{flex: 1}}>
